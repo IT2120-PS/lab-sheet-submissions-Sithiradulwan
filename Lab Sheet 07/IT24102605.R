d44502abb52a0abb3b10ccbda9285005fe127643
Sithiradulwan <it24102605@my.sliit.lk>
2025-09-16 23:58:51 +0530

    IT24102605_lab7

---
+++ Lab Sheet 07/IT24102605.R
@@ -0,0 +1,13 @@
+setwd("C:\\Users\\Local\\Desktop\\IT24102605")
+
+
+punif(25, min = 0, max = 40) - punif(10, min = 0, max = 40)
+
+
+pexp(2, rate = 1/3, lower.tail = TRUE)
+
+
+pnorm(130, mean = 100, sd = 15, lower.tail = FALSE)
+
+
+qnorm(0.95, mean = 100, sd = 15, lower.tail = TRUE)
